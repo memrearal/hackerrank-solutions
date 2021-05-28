39f610758a5044a508e3d8edb656fb044d8acd13
Emre <50625747+memrearal@users.noreply.github.com>
2021-05-28 18:12:51 +0300

    Create day1-let-and-const.js

---
+++ 10-days-of-javascript/day1-let-and-const.js
@@ -0,0 +1,13 @@
+function main() {
+    
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r=readLine();
+    const PI=Math.PI;
+    // Print the area of the circle:
+    var area=parseFloat(PI*r*r);
+    console.log(area);
+    // Print the perimeter of the circle:
+    var peri=parseFloat(2*PI*r);
+    console.log(peri);
+
+}
